bbcc40667fc77c516d17110502151809f6f75373
Denta Muhajir <denta.muhajir@binus.ac.id>
2025-12-27 09:59:30 +0700

    Implement consumer to store in service

--- internal/kafka/consumer/create_wallet_consumer.go
+++ internal/kafka/consumer/create_wallet_consumer.go
@@ -2,7 +2,7 @@ package consumer
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
 	"log"
 	"paybridge-transaction-service/internal/config"
 	"paybridge-transaction-service/internal/domain/wallet"
@@ -37,17 +37,28 @@ func (c *WalletCreateConsumer) Start(ctx context.Context) {
 			log.Println("Fetch error:", err)
 			continue
 		}
+ 
+		var event WalletCreateEvent
+		if err := json.Unmarshal(msg.Value, &event); err != nil {
+			log.Println("Invalid message:", err)
+			_ = c.reader.CommitMessages(ctx, msg) // skip bad message
+			continue
+		}
 
-		//c.Service.CreateWallet(ctx, wallet.CreateWalletReq{})
+		req := wallet.CreateWalletReq{
+			UserID:   event.UserID,
+			Currency: event.Currency,
+		}
 
-		fmt.Printf(
-			"[Kafka] topic=%s key=%s value=%s offset=%d\n",
-			msg.Topic,
-			string(msg.Key),
-			string(msg.Value),
-			msg.Offset,
-		)
+		if _, err := c.Service.CreateWallet(ctx, req); err != nil {
+			log.Println("Create wallet failed:", err)
+			continue // DO NOT COMMIT → Kafka will retry
+		}
 
-		_ = c.reader.CommitMessages(ctx, msg)
+		if err := c.reader.CommitMessages(ctx, msg); err != nil {
+			log.Println("Commit failed:", err)
+		}
+
+		log.Printf("[Kafka] wallet created user=%s currency=%s", event.UserID, event.Currency)
 	}
 }
