c20276e54b70306e66c135c8c999e09d95aa9574
Denta Muhajir <denta.muhajir@binus.ac.id>
2026-02-08 18:06:32 +0700

    feat: add otel & setup

---
+++ internal/infra/otel/tracer.go
@@ -0,0 +1,43 @@
+package otel
+
+import (
+	"context"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
+	"google.golang.org/grpc"
+)
+
+func InitTracer(ctx context.Context, serviceName string) (func(context.Context) error, error) {
+	exporter, err := otlptracegrpc.New(
+		ctx,
+		otlptracegrpc.WithEndpoint("paybridge-jaeger:4317"),
+		otlptracegrpc.WithInsecure(),
+		otlptracegrpc.WithDialOption(grpc.WithBlock()),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := resource.New(
+		ctx,
+		resource.WithAttributes(
+			semconv.ServiceName(serviceName),
+		),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exporter),
+		sdktrace.WithResource(res),
+	)
+
+	otel.SetTracerProvider(tp)
+
+	return tp.Shutdown, nil
+}
--- internal/server/router.go
+++ internal/server/router.go
@@ -4,21 +4,22 @@ import (
 	"paybridge-transaction-service/docs"
 	"paybridge-transaction-service/internal/account"
 	"paybridge-transaction-service/internal/health"
-	"paybridge-transaction-service/internal/loan"
-	"paybridge-transaction-service/internal/server/middleware"
-	"paybridge-transaction-service/internal/wallet"
+	"paybridge-transaction-service/internal/infra/logger"
+
+	"go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/labstack/echo/v4"
 	echoSwagger "github.com/swaggo/echo-swagger"
-	"go.uber.org/zap"
 )
 
-func NewRouter(db *pgxpool.Pool, log *zap.Logger) *echo.Echo {
+func NewRouter(db *pgxpool.Pool, log *logger.Logger) *echo.Echo {
 
 	e := echo.New()
 
-	e.Use(middleware.TracingMiddleware())
+	e.Use(otelecho.Middleware("paybridge-transaction-service"))
+
+	//e.Use(middleware.TracingMiddleware())
 
 	apiVersion := "/api/v1"
 
@@ -39,15 +40,15 @@ func NewRouter(db *pgxpool.Pool, log *zap.Logger) *echo.Echo {
 	healthHandler := health.NewHandler(*healthService)
 	healthHandler.RegisterRoutes(e.Group(apiVersion))
 
-	walletRepo := wallet.NewRepository(db, log)
-	walletService := wallet.NewService(walletRepo, log)
-	walletHandler := wallet.NewHandler(walletService, log)
-	walletHandler.RegisterRoutes(e.Group(apiVersion))
+	// walletRepo := wallet.NewRepository(db, log)
+	// walletService := wallet.NewService(walletRepo, log)
+	// walletHandler := wallet.NewHandler(walletService, log)
+	// walletHandler.RegisterRoutes(e.Group(apiVersion))
 
-	loanAppsRepo := loan.NewRepository(db, log)
-	loanAppsSvc := loan.NewService(loanAppsRepo, log)
-	loanAppsHandler := loan.NewHandler(loanAppsSvc, log)
-	loanAppsHandler.RegisterRoutes(e.Group((apiVersion)))
+	// loanAppsRepo := loan.NewRepository(db, log)
+	// loanAppsSvc := loan.NewService(loanAppsRepo, log)
+	// loanAppsHandler := loan.NewHandler(loanAppsSvc, log)
+	// loanAppsHandler.RegisterRoutes(e.Group((apiVersion)))
 
 	accountRepo := account.NewRepository(db, log)
 	accountSvc := account.NewService(accountRepo, log)
